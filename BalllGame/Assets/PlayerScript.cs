b3988a21931edb34f4730637f4f59c65bb0f9625
ksagane <gusyazero@gmail.com>
2014-03-11 08:36:38 +0900

    add drag function

--- BalllGame/Assets/PlayerScript.cs
+++ BalllGame/Assets/PlayerScript.cs
@@ -6,6 +6,9 @@ public class PlayerScript : MonoBehaviour {
 	protected float x = 0.0f;
 	public float speed = 0.2f;
 
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -23,5 +26,16 @@ public class PlayerScript : MonoBehaviour {
 			transform.localScale -= new Vector3(localX, 0.0f, 0.0f);
 			if (transform.localScale.x < 1.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f); 
 		}
+	}
+
+	private void OnMouseDown () {
+		this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+		this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	private void OnMouseDrag () {
+		Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+		transform.position = currentPosition;
 	}
 }
